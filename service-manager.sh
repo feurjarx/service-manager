d1664aff32e5d0661b27ea6e2c304f8e871ee5f6
feurjarx <feurjarx@gmail.com>
2017-11-14 16:58:02 +0700

    Create service-manager.sh

---
+++ service-manager.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -a
+if [ "$3" != "" ]; then . $3; else . .env.development; fi
+set +a
+
+function __usage ()
+{
+  echo "Usage : $0 <service-type> <action>"
+  echo "ex.: $0 mysql deploy"
+  exit
+}
+
+executer="./docker-services/$1/$2.sh"
+if [ -f "$executer" ]
+then
+    echo sh $executer
+    sh $executer
+else
+	echo "[x] Error! Command is not supported."
+	__usage
+fi
